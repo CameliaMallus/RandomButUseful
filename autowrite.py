7e7dcbe145a8f83201781d4b31771791bef1eece
CameliaMallus <cameliamallus@gmail.com>
2022-08-23 13:52:02 +0200

    Added randomness to autowrite.py

--- autowrite.py
+++ autowrite.py
@@ -1,16 +1,20 @@
 import pyautogui as pag
 import pyperclip as ppc
 import time
+import random
 pag.click(240,50)
+#pag.click(170,915)
 pag.click(1750,74)
 def autowrite(file):
     time.sleep(1.5)
     pag.PAUSE=0.0
     for x in file:
         ppc.copy(x)
-        time.sleep(0.00001)
+        time.sleep(random.randint(25,75)/3000) # Increase or decrease according to your setup and the time you want it to last
         pag.hotkey('ctrl','v')
-        time.sleep(0.00001)
+        time.sleep(random.randint(25,75)/3000) # same
+        if random.random()<0.2:
+            time.sleep(0.06)
 file=open("filetowrite.txt", "r")
 autowrite(file.read())
 file.close()
